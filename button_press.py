1937859aba0b0c072250e8473fdfad4409ba53f4
Glenn K. Lockwood <glennklockwood@gmail.com>
2016-09-21 23:32:36 -0700

    housekeeping

--- button_press.py
+++ button_press.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python
 #
-#  Basic demonstration of detecting a physical button press
+#  Basic demonstration of detecting a physical button press using GPIO polling
 #
 
 import RPi.GPIO as GPIO
 import time
 
-# _PIN_IN = 7
-# GPIO.setmode( GPIO.BOARD )
-# GPIO.setup( _PIN_IN, GPIO.IN, pull_up_down=GPIO.PUD_UP )
-
 _PIN_IN = 4
 GPIO.setmode( GPIO.BCM )
 GPIO.setup( _PIN_IN, GPIO.IN, pull_up_down=GPIO.PUD_UP )
 
-while True:
-    input_state = GPIO.input( _PIN_IN )
-    if input_state == False:
-        print( 'Button Pressed' )
-        time.sleep(0.5)
+try:
+    while True:
+        input_state = GPIO.input( _PIN_IN )
+        if input_state == False:
+            print 'button pressed'
+            time.sleep(0.5)
+except KeyboardInterrupt:
+    print "cleaning up"
+    GPIO.cleanup()
